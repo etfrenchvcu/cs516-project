694d753cc46142dfe48a27e16644a753737b2023
Evan French <etfrench@vcu.edu>
2019-02-11 11:09:17 -0500

    Experiementing with sematic type lists

---
+++ semantic_type_lists.py
@@ -0,0 +1,28 @@
+#Author: Evan French
+#MetaMap semantic types corresponding to medical tests
+tests = [
+'amas', #Amino Acid Sequence
+'mbrt', #Molecular Biology Research Technique
+'edac', #Educational Activity
+'irda', #Indicator, Reagent, or Diagnostic Aid
+'ffas', #Fully Formed Anatomical Structure
+]
+#Author: Evan French
+#MetaMap semantic types corresponding to medical treatments
+treatments = [
+'clnd', #Clinical Drug
+'drdd', #Drug Delivery Device
+'orgt', #Organization
+'bodm', #Biomedical or Dental Material
+'mcha', #Machine Activity
+'food', #Food
+'hcpp', #Human-caused Phenomenon or Process
+'tisu', #Tissue
+]
+#Author: Evan French
+#MetaMap semantic types corresponding to medical problems
+problems = [
+'grpa', #Group Attribute
+'amph', #Amphibian
+'mobd', #Mental or Behavioral Dysfunction
+]
