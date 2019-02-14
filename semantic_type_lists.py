674532ad8054947b2f6f520888b44478e1f43a66
Evan <etfrench@vcu.edu>
2019-02-14 11:27:49 -0500

    working

--- semantic_type_lists.py
+++ semantic_type_lists.py
@@ -2,21 +2,6 @@
 #MetaMap semantic types corresponding to medical tests
 tests = [
 'amas', #Amino Acid Sequence
-'mbrt', #Molecular Biology Research Technique
-'edac', #Educational Activity
-'irda', #Indicator, Reagent, or Diagnostic Aid
-'ffas', #Fully Formed Anatomical Structure
-'ocdi', #Occupation or Discipline
-'elii', #Element, Ion, or Isotope
-'enty', #Entity
-'comd', #Cell or Molecular Dysfunction
-'vita', #Vitamin
-'nnon', #Nucleic Acid, Nucleoside, or Nucleotide
-'lbpr', #Laboratory Procedure
-'bacs', #Biologically Active Substance
-'famg', #Family Group
-'resa', #Research Activity
-'lbtr', #Laboratory or Test Result
 ]
 #Author: Evan French
 #MetaMap semantic types corresponding to medical treatments
@@ -29,28 +14,6 @@ treatments = [
 'food', #Food
 'hcpp', #Human-caused Phenomenon or Process
 'tisu', #Tissue
-'topp', #Therapeutic or Preventive Procedure
-'genf', #Genetic Function
-'antb', #Antibiotic
-'medd', #Medical Device
-'acty', #Activity
-'bdsu', #Body Substance
-'bsoj', #Body Space or Junction
-'bpoc', #Body Part, Organ, or Organ Component
-'popg', #Population Group
-'orgf', #Organism Function
-'plnt', #Plant
-'biof', #Biologic Function
-'mamm', #Mammal
-'blor', #Body Location or Region
-'spco', #Spatial Concept
-'anst', #Anatomical Structure
-'gora', #Governmental or Regulatory Activity
-'hlca', #Health Care Activity
-'mnob', #Manufactured Object
-'menp', #Mental Process
-'socb', #Social Behavior
-'phpr', #Phenomenon or Process
 ]
 #Author: Evan French
 #MetaMap semantic types corresponding to medical problems
